d423b7af8f3ed41d06966399b0eb34891e1fe2fb
Anton Asmirko <AntonAsmirko@yandex.ru>
2020-06-12 05:25:34 +0300

    .

--- slae-solver.c
+++ slae-solver.c
@@ -1,52 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <math.h>
 
 #define ld long double
 
 /* utils */
 
-// function to print squere matrix with its free coeficients
-void print_squere_matrix(ld **matrix, int matrix_size, FILE *file)
+int float_equals(ld a, ld b, ld epsilon)
 {
-    for (int i = 0; i < matrix_size; i++)
+    if (fabs(a - b) < epsilon)
     {
-        for (int j = 0; j < matrix_size + 1; j++)
-        {
-            if (j == matrix_size)
-            {
-                fprintf(file, "%Lf\n", matrix[i][j]);
-            }
-            else
-            {
-                fprintf(file, "%Lf ", matrix[i][j]);
-            }
-        }
+        return 1;
+    }
+    else
+    {
+        return 0;
     }
 }
 
 /* some structs for slae */
 
-typedef struct SLAE
-{
-    int size;
-    ld **coeficients;
-} SLAE;
-
 typedef enum answer_type
 {
     no_solutions,
     one_solution,
-    inf_solution
+    inf_solution,
+    not_solved_yet
 } answer_type;
 
-typedef struct SLAE_solution
+typedef struct SLAE
 {
-    int SLAE_size;
+    int size;
+    ld **coeficients;
     answer_type at;
-    ld *solution;
-
-} SLAE_solution;
+} SLAE;
 
 int make_SLAE(int system_size, SLAE *system)
 {
@@ -57,15 +45,7 @@ int make_SLAE(int system_size, SLAE *system)
     }
 
     system->size = system_size;
-
-    return 0;
-}
-
-int make_SLAE_solution(int size, SLAE_solution *slae_solution)
-{
-    slae_solution = (SLAE_solution *)malloc(sizeof(SLAE_solution));
-    slae_solution->SLAE_size = size;
-    slae_solution->solution = (ld *)malloc(sizeof(ld) * size);
+    system->at = not_solved_yet;
 
     return 0;
 }
@@ -95,22 +75,54 @@ int read_input(char *in_file, SLAE *input_system)
     return 0;
 }
 
+/* write output */
+
+int write_output(char *out_file, SLAE *solution)
+{
+    FILE *out;
+    if (!(out = fopen(out_file, "w")))
+    {
+        return 1;
+    }
+    switch (solution->at)
+    {
+    case no_solutions:
+        fprintf(out, "%s", "no solution\n");
+        break;
+    case inf_solution:
+        fprintf(out, "%s", "many solutions\n");
+        break;
+    case one_solution:
+        for (int i = 0; i < solution->size; i++)
+        {
+            fprintf(out, "%Lf\n", solution->coeficients[i][solution->size]);
+        }
+        break;
+    default:
+        printf("%s", "strange behavior, troubles with solution cathegory\n");
+        fclose(out);
+        return 2;
+    }
+    fclose(out);
+    return 0;
+}
+
 /* slae solver */
 
-void matrix_change_row(ld **matrix, const ld *new_row, const int pos)
+void matrix_change_row(ld **matrix, ld *new_row, int pos)
 {
     ld *to_erase = matrix[pos];
     matrix[pos] = new_row;
     free(to_erase);
 }
 
-int max_elt_pos(const SLAE *slae, const int column)
+int max_elt_pos(SLAE *slae, int column)
 {
     ld max = slae->coeficients[column][column];
     int max_pos = column;
     for (int i = column; i < slae->size; i++)
     {
-        if (slae->coeficients[i][column] > max)
+        if (slae->coeficients[i][column] != 0 && (slae->coeficients[i][column] > max || max == 0))
         {
             max = slae->coeficients[i][column];
             max_pos = i;
@@ -119,14 +131,14 @@ int max_elt_pos(const SLAE *slae, const int column)
     return max_pos;
 }
 
-void row_swap(const ld **arr, const int first_row_index, const int second_row_index)
+void row_swap(ld **arr, int first_row_index, int second_row_index)
 {
     ld *tmp = arr[first_row_index];
     arr[first_row_index] = arr[second_row_index];
     arr[second_row_index] = tmp;
 }
 
-ld *vector_times_scalar(const ld *vector, const int vector_size, const ld scalar)
+ld *vector_times_scalar(ld *vector, int vector_size, ld scalar)
 {
     ld *result = (ld *)malloc(sizeof(ld) * vector_size);
     for (int i = 0; i < vector_size; i++)
@@ -136,7 +148,7 @@ ld *vector_times_scalar(const ld *vector, const int vector_size, const ld scalar
     return result;
 }
 
-ld *vector_subtruction(const ld *vector1, const ld *vector2, const int vector_size)
+ld *vector_subtruction(ld *vector1, ld *vector2, int vector_size)
 {
     ld *result = (ld *)malloc(sizeof(ld) * vector_size);
     for (int i = 0; i < vector_size; i++)
@@ -146,7 +158,7 @@ ld *vector_subtruction(const ld *vector1, const ld *vector2, const int vector_si
     return result;
 }
 
-void eliminate_column(const ld **matrix, const int matrix_size, const int column, const int pivot_index)
+void eliminate_column(ld **matrix, int matrix_size, int column, int pivot_index)
 {
     ld *norm_pivot = vector_times_scalar(matrix[pivot_index], matrix_size + 1, 1.0 / matrix[pivot_index][column]);
     matrix_change_row(matrix, norm_pivot, pivot_index);
@@ -166,36 +178,60 @@ void eliminate_column(const ld **matrix, const int matrix_size, const int column
     }
 }
 
-/* 
-    Enshures that there are no zeros
+/*
+    Ensures that there are no zeros
         in main diagonal of matrix.
     We will choose for pivot max by absolute value
-        element in column. 
+        element in column.
  */
-void pivoting(SLAE *slae)
+int pivoting(SLAE *slae)
 {
+    int was_skip = 0;
     for (int i = 0; i < slae->size; i++)
     {
         int max_pos = max_elt_pos(slae, i);
+        if (slae->coeficients[max_pos][i] == 0.0)
+        {
+            was_skip = 1;
+            continue;
+        }
         if (max_pos != i)
         {
             row_swap(slae->coeficients, max_pos, i);
         }
         eliminate_column(slae->coeficients, slae->size, i, i);
     }
+    return was_skip;
 }
 
-// SLAE_solution *solve_SLAE(SLAE *slae)
-// {
-
-// }
-
-void test_pivoting(SLAE *slae)
+answer_type check_solution(SLAE *slae)
 {
-    FILE *test = fopen("test.txt", "w");
-    pivoting(slae);
-    print_squere_matrix(slae->coeficients, slae->size, test);
-    fclose(test);
+    for (int i = 0; i < slae->size; i++)
+    {
+        ld result = 0.0;
+        for (int j = 0; j < slae->size; j++)
+        {
+            result += slae->coeficients[i][j] * slae->coeficients[j][slae->size];
+        }
+        if (!float_equals(result, slae->coeficients[i][slae->size], 0.000001))
+        {
+            return no_solutions;
+        }
+    }
+    return inf_solution;
+}
+
+void solve_SLAE(SLAE *slae)
+{
+    int was_skip = pivoting(slae);
+    if (was_skip)
+    {
+        slae->at = check_solution(slae);
+    }
+    else
+    {
+        slae->at = one_solution;
+    }
 }
 
 /* main */
@@ -205,18 +241,22 @@ int main(int argc, char **argv)
 
     if (argc != 3)
     {
-        printf("%s%d%s", "wrong number of arguments passed to program, got ", argc - 1, ", expected 2");
+        printf("%s%d%s", "wrong number of arguments passed to program, got ", argc - 1, ", expected 2\n");
         exit(1);
     }
 
     SLAE system;
     if (read_input(argv[1], &system) == 1)
     {
-        printf("%s", "error while opening input file");
+        printf("%s", "error while opening input file\n");
         exit(1);
     };
+    solve_SLAE(&system);
 
-    test_pivoting(&system);
-
+    if (write_output(argv[2], &system))
+    {
+        printf("%s", "an error occured while attempt to write solution to file\n");
+        exit(1);
+    }
     return 0;
 }
